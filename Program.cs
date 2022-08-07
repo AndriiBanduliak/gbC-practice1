925ef0eeb38d4c082be0448082c83c6587832a8e
Andrii Banduliak <banduliak1303@gmail.com>
2022-08-07 13:52:26 +0300

    task 1 is done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Write("Enter the number one ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the number two ");
+int num2 = int.Parse(Console.ReadLine());
+
+if(num1 > num2)
+Console.WriteLine(" The number one is greater then the number two ");
+else
+Console.WriteLine("The number two is greater then the number two");
+
