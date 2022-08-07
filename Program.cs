350f1e96e443a19ccc6341434107daca0389bf41
Andrii Banduliak <banduliak1303@gmail.com>
2022-08-07 13:11:46 +0300

    i have done practice 1-1

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
-﻿//Practice 1 -1
-using static System.Console;
+﻿//Practice 1 -2
+Console.Write("Enter the number one ");
+int num1 = int.Parse(Console.ReadLine());
 
-Clear();
-Write("Enter the number ");
-int number = Convert.ToInt32(ReadLine());
-int sqr = number*number;
-WriteLine($"Square of the {number} is {sqr}");
+Console.Write("Enter the number two ");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num2*num2 == num1)
+Console.WriteLine("number one is a square of number two ");
+else
+Console.WriteLine("number one is not a square of the number two ");
+
+
+
+
+
