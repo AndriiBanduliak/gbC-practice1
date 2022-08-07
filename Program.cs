e7003a5debb20de72b86cac4d36eb47dcfa19aaa
Andrii Banduliak <banduliak1303@gmail.com>
2022-08-07 12:21:31 +0300

    square of the number

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, !!!World!");
+﻿//Practice 1 -1
+using static System.Console;
+
+Clear();
+Write("Enter the number ");
+int number = Convert.ToInt32(ReadLine());
+int sqr = number*number;
+WriteLine($"Square of the {number} is {sqr}");
